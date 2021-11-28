a5d558b7acf836fc9aa3fe325e577a1b33475803
Mrhaltung <dipeshchandekar99@gmail.com>
2021-11-28 13:56:25 +0530

    Mood Analyzer Program to Respond happy or sad mood

---
+++ Mood_Analyzer_Program/MoodAnalyzer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mood_Analyzer_Program
+{
+    public class MoodAnalyzer
+    {        
+        public string AnalyseMood(string msg)
+        {
+            if (msg.ToLower().Contains("sad"))
+                return "SAD";
+           
+            else
+                return "HAPPY";
+        }
+    }
+}
---
+++ Mood_Analyzer_Test/TestMood.cs
@@ -0,0 +1,33 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mood_Analyzer_Program;
+namespace Mood_Analyzer_Test
+{
+    [TestClass]
+    public class TestMood
+    {
+        [TestMethod]
+        public void MsgSadMood()
+        {
+            string msg = "I am in sad mood";
+            string expected = "SAD";
+
+            MoodAnalyzer mood = new MoodAnalyzer();
+
+            string actual = mood.AnalyseMood(msg);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void MsgHaapyMood()
+        {
+            string msg = "I am in Any mood";
+            string expected = "HAPPY";
+
+            MoodAnalyzer mood = new MoodAnalyzer();
+
+            string actual = mood.AnalyseMood(msg);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
