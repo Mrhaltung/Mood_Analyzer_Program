85991132a74e62c277ed1baa031088cf7c75a182
Mrhaltung <dipeshchandekar99@gmail.com>
2021-11-28 13:40:38 +0530

    Add project files.

---
+++ Mood_Analyzer_Program/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Mood_Analyzer_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO MOOD ANALYZER PROGRAM");
+          
+        }
+    }
+}
